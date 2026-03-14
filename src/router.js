df45a81fa81ccdf051d5cda98363e7382eba692a
Chokri <kadoussi.chokri@gmail.com>
2026-03-14 23:04:26 +0100

    TOUR-18 feat(router): mount tournaments router

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import competitorRouter from './competitors/competitors.route.js';
+import tournamentsRouter from './tournaments/tournaments.route.js';
 
 const router = Router();
 
 router.use('/competitors', competitorRouter);
+router.use('/tournaments', tournamentsRouter);
 
 export default router;
