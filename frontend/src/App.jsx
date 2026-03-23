1d29214df44acd55a165eb5cc436d5b9948de805
Chokri <kadoussi.chokri@gmail.com>
2026-03-23 12:56:41 +0100

    TOUR-90 feat(frontend): add nav with NavLink and active class for tournaments and competitors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Routes, Route, Navigate} from 'react-router-dom';
+import {Routes, Route, Navigate, NavLink} from 'react-router-dom';
 import Competitors from "@/pages/Competitors.jsx";
 import Tournaments from "@/pages/Tournaments.jsx";
 import TournamentDetail from "@/pages/TournamentDetail.jsx";
@@ -6,11 +6,18 @@ import TournamentDetail from "@/pages/TournamentDetail.jsx";
 function App() {
   return (
     <>
+
+      <nav>
+        <NavLink to="/tournaments" className={({isActive}) => isActive ? 'active' : ''}>Tournois</NavLink>
+        <NavLink to="/competitors" className={({isActive}) => isActive ? 'active' : ''}>Compétiteurs</NavLink>
+      </nav>
+
+
       <Routes>
         <Route path="/" element={<Navigate to="/tournaments"/>}/>
-        <Route path="/tournaments" element={<Tournaments />} />
-        <Route path="/tournaments/:id" element={<TournamentDetail />} />
-        <Route path="/competitors" element={<Competitors />} />
+        <Route path="/tournaments" element={<Tournaments/>}/>
+        <Route path="/tournaments/:id" element={<TournamentDetail/>}/>
+        <Route path="/competitors" element={<Competitors/>}/>
       </Routes>
     </>
   );
