c0b1977b7df06908ed4e5ae274423d9d691bc21d
Chokri <kadoussi.chokri@gmail.com>
2026-03-24 17:09:21 +0100

    chore: reformatter main.jsx et mettre à jour package-lock.json

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
@@ -13,12 +13,12 @@ export const queryClient = new QueryClient({
     },
   },
 });
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </QueryClientProvider>,
-);
+ReactDOM.createRoot(document.getElementById('root'))
+  .render(
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </QueryClientProvider>,
+  );
